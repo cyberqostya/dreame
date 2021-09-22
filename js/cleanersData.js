2b09a5be9a5d3c6e9b5c068028824a4ac943019b
unknown <mikoffmikoff@mail.ru>
2021-09-22 18:37:21 +0300

    card in html to js component

---
+++ js/cleanersData.js
@@ -0,0 +1,215 @@
+const cleaners = {
+  wireless: [
+    {
+      name: 'V12',
+      images: [
+        { link: './img/wireless/V12/1.png', objectFit: 'contain' },
+        { link: './img/wireless/V12/2.png', objectFit: 'contain' },
+        { link: './img/wireless/V12/3.png', objectFit: 'contain' },
+        { link: './img/wireless/V12/4.jpg', objectFit: 'cover' },
+        { link: './img/wireless/V12/5.jpg', objectFit: 'cover' },
+      ],
+      linksOnMarkets: {
+        wildberries: '#',
+        eldorado: '#',
+      },
+      advantages: [
+        { title: 'Время работы', text: '90 мин' },
+        { title: 'Масса корпуса', text: '1.65 кг' },
+        { title: 'Время зарядки', text: '4 ч' },
+        { title: 'Тип уборки', text: 'Сухая' },
+        { title: 'Тип мотора', text: 'Бесщеточный Space 5.0' },
+        { title: 'Мощность всасывания', text: '185 Вт' },
+        { title: 'Емкость аккумулятора', text: '3000 мА*ч' },
+        { title: 'Потребляемая мощность', text: '550 Вт' },
+        { title: 'Объем пылесборника', text: '0,5 л' },
+      ]
+    },
+
+
+    {
+      name: 'T10',
+      images: [
+        { link: './img/wireless/T10/1.jpg', objectFit: 'cover' },
+        { link: './img/wireless/T10/2.jpg', objectFit: 'cover' },
+        { link: './img/wireless/T10/3.jpg', objectFit: 'cover' },
+        { link: './img/wireless/T10/4.jpg', objectFit: 'cover' },
+        { link: './img/wireless/T10/5.jpg', objectFit: 'cover' },
+      ],
+      linksOnMarkets: {
+        wildberries: '#',
+        eldorado: '#',
+      },
+      advantages: [
+        { title: 'Время работы', text: '60 мин' },
+        { title: 'Масса корпуса', text: '1.65 кг' },
+        { title: 'Время зарядки', text: '3.5 ч' },
+        { title: 'Тип уборки', text: 'Сухая' },
+        { title: 'Тип мотора', text: 'Бесщеточный Space 3.0' },
+        { title: 'Мощность всасывания', text: '120 Вт' },
+        { title: 'Емкость аккумулятора', text: '2500 мА*ч' },
+        { title: 'Потребляемая мощность', text: '400 Вт' },
+        { title: 'Объем пылесборника', text: '0,6 л' },
+      ]
+    },
+
+
+    {
+      name: 'V9',
+      images: [
+        { link: './img/wireless/V9/1.jpg', objectFit: 'cover' },
+        { link: './img/wireless/V9/2.png', objectFit: 'contain' },
+        { link: './img/wireless/V9/3.png', objectFit: 'contain' },
+        { link: './img/wireless/V9/4.jpg', objectFit: 'cover' },
+        { link: './img/wireless/V9/5.jpg', objectFit: 'cover' },
+      ],
+      linksOnMarkets: {
+        wildberries: '#',
+        eldorado: '#',
+      },
+      advantages: [
+        { title: 'Время работы', text: '60 мин' },
+        { title: 'Масса корпуса', text: '1.5 кг' },
+        { title: 'Время зарядки', text: '3.5 ч' },
+        { title: 'Тип уборки', text: 'Сухая' },
+        { title: 'Тип мотора', text: 'Бесщеточный Space 3.0' },
+        { title: 'Мощность всасывания', text: '140 Вт' },
+        { title: 'Емкость аккумулятора', text: '2500 мА*ч' },
+        { title: 'Потребляемая мощность', text: '400 Вт' },
+        { title: 'Объем пылесборника', text: '0,5 л' },
+      ]
+    },
+
+
+    {
+      name: 'V10',
+      images: [
+        { link: './img/wireless/V10/1.jpg', objectFit: 'cover' },
+        { link: './img/wireless/V10/2.jpg', objectFit: 'cover' },
+        { link: './img/wireless/V10/3.jpg', objectFit: 'cover' },
+        { link: './img/wireless/V10/4.jpg', objectFit: 'cover' },
+      ],
+      linksOnMarkets: {
+        wildberries: '#',
+        eldorado: '#',
+      },
+      advantages: [
+        { title: 'Время работы', text: '60 мин' },
+        { title: 'Масса корпуса', text: '1.5 кг' },
+        { title: 'Время зарядки', text: '3.5 ч' },
+        { title: 'Тип уборки', text: 'Сухая' },
+        { title: 'Тип мотора', text: 'Бесщеточный Space 3.0' },
+        { title: 'Мощность всасывания', text: '140 Вт' },
+        { title: 'Емкость аккумулятора', text: '2500 мА*ч' },
+        { title: 'Потребляемая мощность', text: '450 Вт' },
+        { title: 'Объем пылесборника', text: '0,5 л' },
+      ]
+    },
+
+
+    {
+      name: 'V11SE',
+      images: [
+        { link: './img/wireless/V11SE/1.png', objectFit: 'contain' },
+        { link: './img/wireless/V11SE/2.png', objectFit: 'contain' },
+        { link: './img/wireless/V11SE/3.jpg', objectFit: 'cover' },
+        { link: './img/wireless/V11SE/4.jpg', objectFit: 'cover' },
+        { link: './img/wireless/V11SE/5.jpg', objectFit: 'cover' },
+      ],
+      linksOnMarkets: {
+        wildberries: '#',
+        eldorado: '#',
+      },
+      advantages: [
+        { title: 'Время работы', text: '90 мин' },
+        { title: 'Масса корпуса', text: '3 кг' },
+        { title: 'Время зарядки', text: '4 ч' },
+        { title: 'Тип уборки', text: 'Сухая' },
+        { title: 'Тип мотора', text: 'Бесщеточный Space 3.0' },
+        { title: 'Мощность всасывания', text: '140 Вт' },
+        { title: 'Емкость аккумулятора', text: '2500 мА*ч' },
+        { title: 'Потребляемая мощность', text: '470 Вт' },
+        { title: 'Объем пылесборника', text: '0,5 л' },
+      ]
+    },
+  ],
+  robot: [
+    {
+      name: 'F9',
+      images: [
+        { link: './img/robot/F9/1.jpg', objectFit: 'cover' },
+        { link: './img/robot/F9/2.jpg', objectFit: 'cover' },
+        { link: './img/robot/F9/3.jpg', objectFit: 'cover' },
+        { link: './img/robot/F9/4.jpg', objectFit: 'cover' },
+      ],
+      linksOnMarkets: {
+        wildberries: '#',
+        eldorado: '#',
+      },
+      advantages: [
+        { title: 'Время работы', text: '150 мин' },
+        { title: 'Масса корпуса', text: '1.5 кг' },
+        { title: 'Габариты', text: '350*350*80 мм' },
+        { title: 'Тип уборки', text: 'Сухая, влажная' },
+        { title: 'Блок для воды', text: '0.2 л' },
+        { title: 'Мощность всасывания', text: '40 Вт' },
+        { title: 'Время зарядки', text: '3 ч' },
+        { title: 'Макс. уровень шума', text: '75 дБ' },
+        { title: 'Объем пылесборника', text: '0,6 л' },
+      ]
+    },
+
+
+    {
+      name: 'D9',
+      images: [
+        { link: './img/robot/D9/1.jpg', objectFit: 'cover' },
+        { link: './img/robot/D9/2.jpg', objectFit: 'cover' },
+        { link: './img/robot/D9/3.jpg', objectFit: 'cover' },
+        { link: './img/robot/D9/4.jpg', objectFit: 'cover' },
+      ],
+      linksOnMarkets: {
+        wildberries: '#',
+        eldorado: '#',
+      },
+      advantages: [
+        { title: 'Время работы', text: '150 мин' },
+        { title: 'Масса корпуса', text: '3.7 кг' },
+        { title: 'Габариты', text: '350*350*96 мм' },
+        { title: 'Тип уборки', text: 'Сухая, влажная' },
+        { title: 'Блок для воды', text: '0.27 л' },
+        { title: 'Мощность всасывания', text: '40 Вт' },
+        { title: 'Время зарядки', text: '3 ч' },
+        { title: 'Макс. уровень шума', text: '75 дБ' },
+        { title: 'Объем пылесборника', text: '0,57 л' },
+      ]
+    },
+
+
+    {
+      name: 'D9 Pro',
+      images: [
+        { link: './img/robot/D9Pro/1.png', objectFit: 'contain' },
+        { link: './img/robot/D9Pro/2.png', objectFit: 'contain' },
+        { link: './img/robot/D9Pro/3.png', objectFit: 'contain' },
+        { link: './img/robot/D9Pro/4.png', objectFit: 'contain' },
+        { link: './img/robot/D9Pro/5.png', objectFit: 'contain' },
+      ],
+      linksOnMarkets: {
+        wildberries: '#',
+        eldorado: '#',
+      },
+      advantages: [
+        { title: 'Время работы', text: '150 мин' },
+        { title: 'Масса корпуса', text: '3.7 кг' },
+        { title: 'Габариты', text: '350*350*96 мм' },
+        { title: 'Тип уборки', text: 'Сухая, влажная' },
+        { title: 'Блок для воды', text: '0.27 л' },
+        { title: 'Мощность всасывания', text: '40 Вт' },
+        { title: 'Время зарядки', text: '3 ч' },
+        { title: 'Макс. уровень шума', text: '75 дБ' },
+        { title: 'Объем пылесборника', text: '0,57 л' },
+      ]
+    },
+  ]
+};
--- js/main.js
+++ js/main.js
@@ -17,48 +17,108 @@ class Slider {
 
 
 class Card {
-  constructor(root) {
+  constructor(props, cardContainer) {
+
+    // Создание карточки путем шаблонной строки
+    this.props = props;
+    this.cardContainer = cardContainer;
+
+    this.stringTemplate = this._getTemplate();
+    this._addCardToCardContainer();
+
+    this.card = document.querySelectorAll('.card');
 
 
-    // Блоки
-
-    this.root = root;
+    // Узлы DOM
+    this.root = this.card[this.card.length - 1];
     this.imagesContainer = this.root.querySelector('.card__images-container'); // Блок с картинками карточки
       this.images = this.imagesContainer.querySelectorAll('.card__image');
       this.imagesControllers = this.imagesContainer.querySelector('.card__images-controllers-container');
         this.imagesControllersTextBlock = this.imagesContainer.querySelector('.card__images-controllers-counter');
-    this.dataContainer = root.querySelector('.card__data-container'); // Блок с данными карточки
-    this.buyButton = root.querySelector('.card__data-button_buy'); // Кнопка КУПИТЬ
-    this.allCharacteristicsButton = root.querySelector('.card__data-button_characteristics'); // Кнопка ВСЕ ХАРАКТЕРИСТИКИ
-    this.dataRowAdvantages = root.querySelector('.card__data-row-advantages'); // Блок с преимуществами в строках
+    this.dataContainer = this.root.querySelector('.card__data-container'); // Блок с данными карточки
+    this.buyButton = this.root.querySelector('.card__data-button_buy'); // Кнопка КУПИТЬ
+    this.allCharacteristicsButton = this.root.querySelector('.card__data-button_characteristics'); // Кнопка ВСЕ ХАРАКТЕРИСТИКИ
+    this.dataRowAdvantages = this.root.querySelector('.card__data-row-advantages'); // Блок с преимуществами в строках
 
 
     // Стейты
-
     this.isOpen = false; // Стейт раскрыта ли карточка
     this.imagesCounter = 1;
 
 
     // Константы
-
-    this.IMAGE_WIDTH_HEIGHT_COEFFICIENT = 1; // высота к ширине
+    this.IMAGE_WIDTH_HEIGHT_COEFFICIENT = 1; // Высота к ширине
 
 
     // Инициализация
-
     this.setHeightToImageContainer();
     this.setInitialPositionForImages();
     this.setEventListenerOnImagesControllers();
   } // ---------------------------------------------------
+
+
+
+
+  // Создание строковой карточки и добавление её в DOM *****
+  _getTemplate() {
+    
+    function _getImage(image) {
+      return `<img src="${image.link}" alt="cleaner" class="card__image card__image_${image.objectFit}">`;
+    }
+
+    function _getAdvantages(advantage) {
+      return `
+        <div class="card__data-row-advantage">
+          <p class="card__data-row-advantage-title">${advantage.title}</p>
+          <p class="card__data-row-advantage-text">${advantage.text}</p>
+        </div>
+      `;
+    }
+
+    const stringImages = this.props.images.map((item) => { return _getImage(item) });
+    const stringAdvantages = this.props.advantages.map((item) => { return _getAdvantages(item) });
+
+    return `
+    <div class="card">
+      <div class="card__images-container">
+        
+        ${stringImages.join(' ')}
+
+        <div class="card__images-controllers-container">
+          <button class="card__images-controller card__images-controller_up"></button>
+          <p class="card__images-controllers-counter">1/${this.props.images.length}</p>
+          <button class="card__images-controller card__images-controller_down"></button>
+        </div>
+      </div>
+      <div class="card__data-container">
+        <h3 class="card__data-title">Пылесос Dreame ${this.props.name}</h3>
+        <div class="card__data-buttons-container">
+          <button class="card__data-button card__data-button_buy">Купить</button>
+          <button class="card__data-button card__data-button_characteristics">Характеристики</button>
+        </div>
+        <div class="card__data-row-advantages">
+          
+          ${stringAdvantages.join(' ')}
+
+        </div>
+      </div>
+    </div>
+    `;
+  }
+
+  _addCardToCardContainer() {
+    this.cardContainer.insertAdjacentHTML('beforeend', this.stringTemplate);
+  }
+  // Конец создания строковой карточки и добавление её в DOM *****
   
 
+  // Задание высоты изображениям карточки
   setHeightToImageContainer() {
     this.imagesContainer.style.height = this.imagesContainer.clientWidth * this.IMAGE_WIDTH_HEIGHT_COEFFICIENT + 'px';
   }
 
 
   // Логика переключения изображений
-
   hideImage(index) { this.images[index].style.opacity = 0 }
   showImage(index) { this.images[index].style.opacity = 1 }
   setControllersText() { this.imagesControllersTextBlock.textContent = `${this.imagesCounter}/${this.images.length}`; }
@@ -87,9 +147,11 @@ class Card {
       }
     });
   }
+  // Конец логики переключения изображений
 
 
 
+  // Здесь будут все функции которые нуждаются в window.resize
   functionsDepenOnWindowSize() {
     this.setHeightToImageContainer();
   }
@@ -112,29 +174,13 @@ class Card {
 const wirelessSlider = new Slider(document.querySelectorAll('.models__slider-road')[0]);
 const robotSlider = new Slider(document.querySelectorAll('.models__slider-road')[1]);
 
-
-const cleanerV12 = new Card(document.querySelectorAll('.card')[0]);
-const cleanerT10 = new Card(document.querySelectorAll('.card')[1]);
-const cleanerV9 = new Card(document.querySelectorAll('.card')[2]);
-const cleanerV10 = new Card(document.querySelectorAll('.card')[3]);
-const cleanerV11SE = new Card(document.querySelectorAll('.card')[4]);
-
-const cleanerF9 = new Card(document.querySelectorAll('.card')[5]);
-const cleanerD9 = new Card(document.querySelectorAll('.card')[6]);
-const cleanerD9Pro = new Card(document.querySelectorAll('.card')[7]);
+const wirelessCleanersArray = [];
+const robotCleanersArray = [];
+cleaners.wireless.forEach((item) => { wirelessCleanersArray.push( new Card(item, wirelessSlider.root) ) }); 
+cleaners.robot.forEach((item) => { robotCleanersArray.push( new Card(item, robotSlider.root) ) }); 
 
 
 window.addEventListener('resize', () => {
-  cleanerV12.functionsDepenOnWindowSize();
-  cleanerT10.functionsDepenOnWindowSize();
-  cleanerV9.functionsDepenOnWindowSize();
-  cleanerV10.functionsDepenOnWindowSize();
-  cleanerV11SE.functionsDepenOnWindowSize();
-
-  cleanerF9.functionsDepenOnWindowSize();
-  cleanerD9.functionsDepenOnWindowSize();
-  cleanerD9Pro.functionsDepenOnWindowSize();
+  wirelessCleanersArray.forEach((item) => { item.functionsDepenOnWindowSize() });
+  robotCleanersArray.forEach((item) => { item.functionsDepenOnWindowSize() });
 });
-
-
-window.scroll({})
